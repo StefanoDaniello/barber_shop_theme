e412b750d212e9fb593e56b60e9357e12fa95cae
StefanoAdinfo <s.daniello@adinfo.it>
2025-05-08 10:52:06 +0200

    hero and sponsor

---
+++ patterns/hero.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Title: Hero
+ * Slug: barber_shop_theme/hero
+ * Categories: barber_shop_theme-hero
+ *
+ * @package barber_shop_theme
+ * @since 1.0.0
+ */
+?>
+
+<!-- wp:group {"style":{"dimensions":{"minHeight":"500px"},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"center"}} -->
+<div class="wp-block-group" style="min-height:500px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:columns {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}}} -->
+    <div class="wp-block-columns" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:column {"verticalAlignment":"bottom","width":"25%"} -->
+        <div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:25%"><!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+            <div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:paragraph {"fontSize":"small"} -->
+                <p class="has-small-font-size">HIGH-QUALITY-SERVICE</p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:image {"id":21,"sizeSlug":"large","linkDestination":"none","style":{"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
+                <figure class="wp-block-image size-large" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0"><img src="http://localhost:10003/wp-content/uploads/2025/05/homepage3-1-1-839x1024.png" alt="" class="wp-image-21" /></figure>
+                <!-- /wp:image -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"width":"50%"} -->
+        <div class="wp-block-column" style="flex-basis:50%"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+            <div class="wp-block-group"><!-- wp:image {"id":20,"width":"300px","sizeSlug":"full","linkDestination":"none"} -->
+                <figure class="wp-block-image size-full is-resized"><img src="http://localhost:10003/wp-content/uploads/2025/05/logo-1.png" alt="" class="wp-image-20" style="width:300px" /></figure>
+                <!-- /wp:image -->
+
+                <!-- wp:heading {"textAlign":"center","level":1} -->
+                <h1 class="wp-block-heading has-text-align-center">MEN'S HAIRCUT <br> &amp; BEARD TRIMMING</h1>
+                <!-- /wp:heading -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"verticalAlignment":"top","width":"25%"} -->
+        <div class="wp-block-column is-vertically-aligned-top" style="flex-basis:25%"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} -->
+            <div class="wp-block-group"><!-- wp:image {"id":22,"sizeSlug":"large","linkDestination":"none"} -->
+                <figure class="wp-block-image size-large"><img src="http://localhost:10003/wp-content/uploads/2025/05/homepage3-2-1-826x1024.png" alt="" class="wp-image-22" /></figure>
+                <!-- /wp:image -->
+
+                <!-- wp:paragraph {"align":"right","fontSize":"small"} -->
+                <p class="has-text-align-right has-small-font-size">HIGH-QUALITY-SERVICE</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
