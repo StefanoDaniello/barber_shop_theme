7641511d9e98c50b391e4e9a1450ded53bbee771
StefanoAdinfo <s.daniello@adinfo.it>
2025-05-09 17:42:09 +0200

    fix

--- patterns/barber-since.php
+++ patterns/barber-since.php
@@ -9,59 +9,65 @@
  * @since 1.0.0
  */
 ?>
-<!-- wp:columns {"style":{"color":{"background":"#fffffff5"}}} -->
-<div class="wp-block-columns has-background" style="background-color:#fffffff5"><!-- wp:column -->
-    <div class="wp-block-column"><!-- wp:image {"id":298,"width":"540px","height":"auto","sizeSlug":"full","linkDestination":"none"} -->
-        <figure class="wp-block-image size-full is-resized"><img src="http://localhost:10003/wp-content/uploads/2025/05/about-1-2_1-removebg-preview.png" alt="" class="wp-image-298" style="width:540px;height:auto" /></figure>
-        <!-- /wp:image -->
-    </div>
-    <!-- /wp:column -->
+<!-- wp:group {"backgroundColor":"contrast","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-contrast-background-color has-background"><!-- wp:columns {"style":{"color":{"background":"#fffffff5"}}} -->
+    <div class="wp-block-columns has-background" style="background-color:#fffffff5"><!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image {"id":298,"width":"511px","height":"auto","sizeSlug":"full","linkDestination":"none"} -->
+            <figure class="wp-block-image size-full is-resized"><img src="http://localhost:10003/wp-content/uploads/2025/05/about-1-2_1-removebg-preview.png" alt="" class="wp-image-298" style="width:511px;height:auto" /></figure>
+            <!-- /wp:image -->
+        </div>
+        <!-- /wp:column -->
 
-    <!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"blockGap":"0"}}} -->
-    <div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"layout":{"type":"constrained"}} -->
-        <div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|card-tag"}}}},"textColor":"card-tag"} -->
-            <p class="has-card-tag-color has-text-color has-link-color">LITTLE STORY</p>
-            <!-- /wp:paragraph -->
-
-            <!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"large"} -->
-            <h2 class="wp-block-heading has-base-color has-text-color has-link-color has-large-font-size">PREMIUM BARBER SERVICE SINCE 1989.</h2>
-            <!-- /wp:heading -->
-
-            <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|card-tag"}}}},"textColor":"card-tag","fontSize":"small"} -->
-            <p class="has-card-tag-color has-text-color has-link-color has-small-font-size">Great Style. Great Haircuts. Awesome Barbers. At Sublime Barbershop, we strive to create the traditional barbershop look and feel with the highest level of skill, service and style to keep you looking and feeling your best.</p>
-            <!-- /wp:paragraph -->
-
-            <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"base"} -->
-            <hr class="wp-block-separator has-text-color has-base-color has-alpha-channel-opacity has-base-background-color has-background" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50)" />
-            <!-- /wp:separator -->
-
-            <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+        <!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"blockGap":"0"}}} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"layout":{"type":"constrained","contentSize":"500px"}} -->
             <div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
-                <div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"small"} -->
-                    <p class="has-base-color has-text-color has-link-color has-small-font-size">Beards Scultping Daily</p>
+                <div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|card-tag"}}}},"textColor":"card-tag","fontSize":"small"} -->
+                    <p class="has-card-tag-color has-text-color has-link-color has-small-font-size">LITTLE STORY</p>
                     <!-- /wp:paragraph -->
 
-                    <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"x-large"} -->
-                    <p class="has-base-color has-text-color has-link-color has-x-large-font-size" style="font-style:normal;font-weight:700">&gt;20</p>
-                    <!-- /wp:paragraph -->
+                    <!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"x-large"} -->
+                    <h2 class="wp-block-heading has-base-color has-text-color has-link-color has-x-large-font-size">PREMIUM BARBER SERVICE SINCE 1989.</h2>
+                    <!-- /wp:heading -->
                 </div>
                 <!-- /wp:group -->
 
-                <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
-                <div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"small"} -->
-                    <p class="has-base-color has-text-color has-link-color has-small-font-size">Clients Every Month</p>
-                    <!-- /wp:paragraph -->
+                <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|card-tag"}}}},"textColor":"card-tag","fontSize":"small"} -->
+                <p class="has-card-tag-color has-text-color has-link-color has-small-font-size">Great Style. Great Haircuts. Awesome Barbers. At Sublime Barbershop, we strive to create the traditional barbershop look and feel with the highest level of skill, service and style to keep you looking and feeling your best.</p>
+                <!-- /wp:paragraph -->
 
-                    <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"x-large"} -->
-                    <p class="has-base-color has-text-color has-link-color has-x-large-font-size" style="font-style:normal;font-weight:700">&gt;450</p>
-                    <!-- /wp:paragraph -->
+                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"base"} -->
+                <hr class="wp-block-separator has-text-color has-base-color has-alpha-channel-opacity has-base-background-color has-background" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50)" />
+                <!-- /wp:separator -->
+
+                <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+                <div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+                    <div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"small"} -->
+                        <p class="has-base-color has-text-color has-link-color has-small-font-size">Beards Scultping Daily</p>
+                        <!-- /wp:paragraph -->
+
+                        <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"x-large"} -->
+                        <p class="has-base-color has-text-color has-link-color has-x-large-font-size" style="font-style:normal;font-weight:700">&gt;20</p>
+                        <!-- /wp:paragraph -->
+                    </div>
+                    <!-- /wp:group -->
+
+                    <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+                    <div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"small"} -->
+                        <p class="has-base-color has-text-color has-link-color has-small-font-size">Clients Every Month</p>
+                        <!-- /wp:paragraph -->
+
+                        <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"x-large"} -->
+                        <p class="has-base-color has-text-color has-link-color has-x-large-font-size" style="font-style:normal;font-weight:700">&gt;450</p>
+                        <!-- /wp:paragraph -->
+                    </div>
+                    <!-- /wp:group -->
                 </div>
                 <!-- /wp:group -->
             </div>
             <!-- /wp:group -->
         </div>
-        <!-- /wp:group -->
+        <!-- /wp:column -->
     </div>
-    <!-- /wp:column -->
+    <!-- /wp:columns -->
 </div>
-<!-- /wp:columns -->
+<!-- /wp:group -->
---
+++ patterns/premium-service.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Title: Premium service
+ * Slug: barber_shop_theme/premium-service
+ * Categories: barber_shop_theme-columns
+ *
+ * @package barber_shop_theme
+ * @since 1.0.0
+ */ ?>
+
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--60);margin-bottom:var(--wp--preset--spacing--60)"><!-- wp:columns -->
+    <div class="wp-block-columns"><!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+            <div class="wp-block-group"><!-- wp:image {"id":322,"sizeSlug":"full","linkDestination":"none"} -->
+                <figure class="wp-block-image size-full"><img src="http://localhost:10003/wp-content/uploads/2025/05/home4-4-1.png" alt="" class="wp-image-322" /></figure>
+                <!-- /wp:image -->
+
+                <!-- wp:image {"id":323,"sizeSlug":"full","linkDestination":"none"} -->
+                <figure class="wp-block-image size-full"><img src="http://localhost:10003/wp-content/uploads/2025/05/home4-5-1.png" alt="" class="wp-image-323" /></figure>
+                <!-- /wp:image -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"verticalAlignment":"center"} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"constrained","contentSize":"500px"}} -->
+            <div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+                <div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|card-tag"}}}},"textColor":"card-tag","fontSize":"small"} -->
+                    <p class="has-card-tag-color has-text-color has-link-color has-small-font-size">WHO WE ARE</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:heading {"fontSize":"x-large"} -->
+                    <h2 class="wp-block-heading has-x-large-font-size">GET FULL RANGE OF <br> PREMIUM SERVICES.</h2>
+                    <!-- /wp:heading -->
+                </div>
+                <!-- /wp:group -->
+
+                <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|card-tag"}}}},"textColor":"card-tag","fontSize":"small"} -->
+                <p class="has-card-tag-color has-text-color has-link-color has-small-font-size">Great Style. Great Haircuts. Awesome Barbers. At Sublime Barbershop, we strive to create the traditional barbershop look and feel with the highest level of skill, service and style to keep you looking and feeling your best.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
