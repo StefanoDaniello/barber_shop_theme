aa92c4458894b30b656f5431074e17c448d4b635
StefanoAdinfo <s.daniello@adinfo.it>
2025-05-09 16:38:33 +0200

    add color and fix pricing

--- patterns/pricing-plans.php
+++ patterns/pricing-plans.php
@@ -9,15 +9,14 @@
  * @since 1.0.0
  */
 ?>
-
-<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
     <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--60)"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|card-tag"}}}},"textColor":"card-tag","fontSize":"small"} -->
         <p class="has-card-tag-color has-text-color has-link-color has-small-font-size">CHOSE YOUR BEST</p>
         <!-- /wp:paragraph -->
 
         <!-- wp:heading {"fontSize":"x-large"} -->
-        <h2 class="wp-block-heading has-x-large-font-size">EXPLORE OUR <br /> OPTIMAL PRICING PLANS.</h2>
+        <h2 class="wp-block-heading has-x-large-font-size">EXPLORE OUR <br> OPTIMAL PRICING PLANS.</h2>
         <!-- /wp:heading -->
     </div>
     <!-- /wp:group -->
@@ -40,8 +39,8 @@
                 <p class="has-card-tag-color has-text-color has-link-color has-small-font-size">Classic Haircut</p>
                 <!-- /wp:paragraph -->
 
-                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"color":{"background":"#191919"}}} -->
-                <hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);background-color:#191919;color:#191919" />
+                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"backgroundColor":"separetor-color"} -->
+                <hr class="wp-block-separator has-text-color has-separetor-color-color has-alpha-channel-opacity has-separetor-color-background-color has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)" />
                 <!-- /wp:separator -->
             </div>
             <!-- /wp:group -->
@@ -62,8 +61,8 @@
                 <p class="has-card-tag-color has-text-color has-link-color has-small-font-size">Classic Haircut</p>
                 <!-- /wp:paragraph -->
 
-                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"color":{"background":"#191919"}}} -->
-                <hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);background-color:#191919;color:#191919" />
+                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"backgroundColor":"separetor-color"} -->
+                <hr class="wp-block-separator has-text-color has-separetor-color-color has-alpha-channel-opacity has-separetor-color-background-color has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)" />
                 <!-- /wp:separator -->
             </div>
             <!-- /wp:group -->
@@ -84,8 +83,8 @@
                 <p class="has-card-tag-color has-text-color has-link-color has-small-font-size">Classic Haircut</p>
                 <!-- /wp:paragraph -->
 
-                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"color":{"background":"#191919"}}} -->
-                <hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);background-color:#191919;color:#191919" />
+                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"backgroundColor":"separetor-color"} -->
+                <hr class="wp-block-separator has-text-color has-separetor-color-color has-alpha-channel-opacity has-separetor-color-background-color has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)" />
                 <!-- /wp:separator -->
             </div>
             <!-- /wp:group -->
@@ -106,8 +105,8 @@
                 <p class="has-card-tag-color has-text-color has-link-color has-small-font-size">Classic Haircut</p>
                 <!-- /wp:paragraph -->
 
-                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"color":{"background":"#191919"}}} -->
-                <hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);background-color:#191919;color:#191919" />
+                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"backgroundColor":"separetor-color"} -->
+                <hr class="wp-block-separator has-text-color has-separetor-color-color has-alpha-channel-opacity has-separetor-color-background-color has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)" />
                 <!-- /wp:separator -->
             </div>
             <!-- /wp:group -->
@@ -131,8 +130,8 @@
                 <p class="has-card-tag-color has-text-color has-link-color has-small-font-size">Classic Haircut</p>
                 <!-- /wp:paragraph -->
 
-                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"color":{"background":"#191919"}}} -->
-                <hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);background-color:#191919;color:#191919" />
+                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"backgroundColor":"separetor-color"} -->
+                <hr class="wp-block-separator has-text-color has-separetor-color-color has-alpha-channel-opacity has-separetor-color-background-color has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)" />
                 <!-- /wp:separator -->
             </div>
             <!-- /wp:group -->
@@ -153,8 +152,8 @@
                 <p class="has-card-tag-color has-text-color has-link-color has-small-font-size">Classic Haircut</p>
                 <!-- /wp:paragraph -->
 
-                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"color":{"background":"#191919"}}} -->
-                <hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);background-color:#191919;color:#191919" />
+                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"backgroundColor":"separetor-color"} -->
+                <hr class="wp-block-separator has-text-color has-separetor-color-color has-alpha-channel-opacity has-separetor-color-background-color has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)" />
                 <!-- /wp:separator -->
             </div>
             <!-- /wp:group -->
@@ -175,8 +174,8 @@
                 <p class="has-card-tag-color has-text-color has-link-color has-small-font-size">Classic Haircut</p>
                 <!-- /wp:paragraph -->
 
-                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"color":{"background":"#191919"}}} -->
-                <hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);background-color:#191919;color:#191919" />
+                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"backgroundColor":"separetor-color"} -->
+                <hr class="wp-block-separator has-text-color has-separetor-color-color has-alpha-channel-opacity has-separetor-color-background-color has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)" />
                 <!-- /wp:separator -->
             </div>
             <!-- /wp:group -->
@@ -197,8 +196,8 @@
                 <p class="has-card-tag-color has-text-color has-link-color has-small-font-size">Classic Haircut</p>
                 <!-- /wp:paragraph -->
 
-                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"color":{"background":"#191919"}}} -->
-                <hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);background-color:#191919;color:#191919" />
+                <!-- wp:separator {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"backgroundColor":"separetor-color"} -->
+                <hr class="wp-block-separator has-text-color has-separetor-color-color has-alpha-channel-opacity has-separetor-color-background-color has-background" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)" />
                 <!-- /wp:separator -->
             </div>
             <!-- /wp:group -->
